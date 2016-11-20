fa18de34f8cc0692ff9e5fb49d0ec6a47d83ef8d
itchenyi <itchenyi@gmail.com>
2016-11-20 19:08:28 +0800

    change the demo

--- get_free_endpoint.py
+++ get_free_endpoint.py
@@ -1,3 +1,4 @@
+import socket
 # Getting a random free tcp port in python using sockets
 
 def get_free_tcp_port():
@@ -14,3 +15,25 @@ def get_free_tcp_address():
     host, port = tcp.getsockname()
     tcp.close()
     return 'tcp://{host}:{port}'.format(**locals())
+
+
+def get_free_ports(howMany=10):
+    """Return a list of n free port numbers on localhost"""
+    results = []
+    sockets = []
+    for x in range(howMany):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('localhost', 0))
+        # work out what the actual port number it's bound to is
+        addr, port = s.getsockname()
+        results.append(port)
+        sockets.append(s)
+
+    for s in sockets:
+        s.close()
+
+    return results
+
+print(get_free_tcp_port())
+print(get_free_tcp_address())
+print(get_free_ports())
